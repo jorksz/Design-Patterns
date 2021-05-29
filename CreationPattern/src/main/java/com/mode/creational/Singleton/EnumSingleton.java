3981bea6fab93cd793a0c2da827fd4a3c72df0f2
lijt <1223360222@qq.com>
2021-05-29 16:11:51 +0800

    单例设计模式

---
+++ CreationPattern/src/main/java/com/mode/creational/Singleton/EnumSingleton.java
@@ -0,0 +1,9 @@
+package com.mode.creational.Singleton;
+
+public enum EnumSingleton {
+    SINGLETON;
+
+    public void test() {
+        System.out.println("hah");
+    }
+}
---
+++ CreationPattern/src/main/java/com/mode/creational/Singleton/Main.java
@@ -0,0 +1,9 @@
+package com.mode.creational.Singleton;
+
+public class Main {
+
+    public static void main(String[] args) {
+        EnumSingleton.SINGLETON.test();
+        System.out.println(DoubleCheckSingleton.getInstance());
+    }
+}
