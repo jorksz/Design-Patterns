e775afd34febeb79ec3b8e12fc74a90f976af2b1
lijt <1223360222@qq.com>
2021-07-10 18:10:29 +0800

    模板设计模式

---
+++ CreationPattern/src/main/java/com/mode/behavioral/template/CheckMain.java
@@ -0,0 +1,11 @@
+package com.mode.behavioral.template;
+
+public class CheckMain {
+
+    public static void main(String[] args) {
+        AbstractCheck check = new ClothesChecked();
+        check.sell();
+        check = new FruitChecked();
+        check.sell();
+    }
+}
